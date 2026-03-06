02f412b145f1cae8da6c94fb1b00a710207f97c1
jushoo <denniel.joshua@pm.me>
2026-03-06 08:58:30 +0800

    initial implementation of processor business logic

--- src/processor.js
+++ src/processor.js
@@ -2,28 +2,18 @@ const { convertToUSD } = require("./lib/currencyConverter.js");
 const { calculateTax } = require("./lib/taxCalculator.js");
 
 function processSales(salesData) {
-  // TODO: Implement business logic here.
-  // 1. Loop through sales
-  // 2. Convert currency to USD
-  // 3. Calculate tax based on type
-  // 4. Return summary object { totalItems, totalRevenue, totalTax }
-  //
-  console.log(salesData);
-
   for (const row of salesData) {
-    const priceToUSD = convertToUSD(row.price);
+    const priceToUSD = convertToUSD(row);
     row.priceToUSD = priceToUSD;
 
     const tax = calculateTax(row);
     row.tax = tax;
   }
 
-  console.log(salesData);
-
   return {
-    totalItems: 0,
-    totalRevenue: 0.0,
-    totalTax: 0.0,
+    totalItems: salesData.length,
+    totalRevenue: salesData.reduce((acc, curr) => acc + curr.priceToUSD, 0),
+    totalTax: salesData.reduce((acc, curr) => acc + curr.tax, 0),
   };
 }
 
